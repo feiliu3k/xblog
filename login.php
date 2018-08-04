69fdcf58f93607ced841477ad35ee2774b2600e1
feiliu3k <1104829018@qq.com>
2018-08-05 00:58:54 +0800

    finish UI

--- login.php
+++ login.php
@@ -1,162 +1,104 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <title>login</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/lst.css" rel="stylesheet">
+    <title>登陆 - 汕头市广播电视台</title>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
- 
-    <style type="text/css" rel="stylesheet">
-        .login-box {
-          width: 100%;
-          max-width:500px;
-          height: 400px;
-          position: absolute;
-          top: 50%;
-          margin-top: -200px;
-          /*设置负值，为要定位子盒子的一半高度*/
-          
-        }
+    <!-- Styles -->
+    <link href="./public/css/app.css" rel="stylesheet">
+</head>
 
-        @media screen and (min-width:500px){
-          .login-box {
-            left: 50%;
-            /*设置负值，为要定位子盒子的一半宽度*/
-            margin-left: -250px;
-          }
-        } 
-
-        .form {
-          width: 100%;
-          max-width:500px;
-          height: 275px;
-          margin: 25px auto 0px auto;
-          padding-top: 25px;
-        } 
-        .login-content {
-          height: 300px;
-          width: 100%;
-          max-width:500px;
-          background-color: rgba(255, 250, 2550, .6);
-          float: left;
-        }
-        .input-group {
-          margin: 0px 0px 30px 0px !important;
-        }
-        .form-control,.input-group {
-          height: 40px;
-        }
-        .form-group {
-          margin-bottom: 0px !important;
-        }
-        .login-title {
-          padding: 20px 10px;
-          background-color: rgba(0, 0, 0, .6);
-        }
-        .login-title h1 {
-          margin-top: 10px !important;
-        }
-        .login-title small {
-          color: #fff;
-        }
-
-        .link p {
-          line-height: 20px;
-          margin-top: 30px;
-        }
-        .btn-sm {
-          padding: 8px 24px !important;
-          font-size: 16px !important;
-        }
-        .alert {
-          box-shadow: 4px 4px 5px 0 rgba(50,50,50,.19);
-        }
-        .alert-success {
-          color: red;
-          background-color: #dff0d8;
-          border-color: #d6e9c6;
-        } 
-    </style>
-  </head>
-  <body id="body">
-    <div id="wrap" >
-        <div  class="navbar navbar-default navbar-static-top">
+<body>
+    <div id="app" class="login-page">
+        <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-                   <a href="/" class="navbar-brand"><b>XBLog</b></a>
-                </div>
-                <div id="top-navbar-collapse" class="navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class=""><a href="#">首页</a></li>        
-                        <li class=""><a href="#">关于</a></li>        
-                    </ul>
-                   
-                </div>
-            </div>
-        </div> 
-        <?php 
-             if (isset($_SESSION['msg'])){
 
-               echo '<div class="container">
-                      <div class="box text-center alert alert-success">
-                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                      <?php echo $_SESSION["msg"];?>
-                      </div>
-                   </div>';
-               
-             }
-           ?>
-        
-       
-        <div class="login_box">
-          <div class="login-box">
-              <div class="login-title text-center">
-                  <h1><small>登录</small></h1>
-              </div>
-              <div class="login-content ">
-              <div class="form">
-              <form action="src/validateUser.php" method="post">
-                  <div class="form-group">
-                      <div class="col-xs-12  ">
-                          <div class="input-group">
-                              <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                              <input type="text" id="username" name="username" class="form-control" placeholder="用户名">
-                          </div>
-                      </div>
-                  </div>
-                  <div class="form-group">
-                      <div class="col-xs-12  ">
-                          <div class="input-group">
-                              <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                              <input type="password" id="password" name="password" class="form-control" placeholder="密码">
-                          </div>
-                      </div>
-                  </div>
-                  <div class="form-group form-actions">
-                      <div class="col-xs-4 col-xs-offset-4 ">
-                          <button type="submit" class="btn btn-sm btn-info"><span class="glyphicon glyphicon-off"></span> 登录</button>
-                      </div>
-                  </div>
-                  
-              </form>
-              </div>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="#">
+                        XBLog
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        <li><a href="./login.php">登录</a></li>
+                     </ul>
+                </div>
             </div>
-          </div>
-        </div>     
+        </nav>
+        <div class="container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">登录</div>
+
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="post" action="./src/validateUser.php">
+                                    
+                                    <div class="form-group">
+                                        <label for="username" class="col-md-4 control-label">用户名</label>
+
+                                        <div class="col-md-6">
+                                            <input id="username" type="text" class="form-control" name="username" value="" required autofocus>
+                                         </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password" class="col-md-4 control-label">密码</label>
+
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control" name="password" required>
+                                        </div>
+                                    </div>
+
+                                    
+
+                                    <div class="form-group">
+                                        <div class="col-md-8 col-md-offset-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                登录
+                                            </button>                                          
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">
+                    由 XMAN 设计和编码
+                </p>
+
+                <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+            </div>
+        </footer>  
     </div>
 
-    
+    <!-- Scripts -->
+    <script src="./public/js/app.js"></script>
 </body>
 </html>
---
+++ webload.php
@@ -0,0 +1,197 @@
+<?php
+
+	session_start();
+
+    if ((!isset($_SESSION['userinfo']))){
+        header('Location: index.php');
+    }
+
+	require __DIR__.'/bootstrap/autoload.php';
+	require __DIR__.'./SqlsrvDao.php';
+	require __DIR__.'./LogFile.php';
+	require __DIR__.'./s_time.php';
+	
+	use Carbon\Carbon;
+	use ZenEnv\ZenEnv;
+
+	header("Content-type: text/html; charset=utf-8"); 
+	
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->load();
+	//$dotenv->overload();
+
+	$env = new ZenEnv(__DIR__.'./.env');	
+		
+	try {		
+   
+        $db_host = getenv('DB_HOST');   
+		$db_name = getenv('DB_DATABASE');
+		$db_url = "sqlsrv:Server=".$db_host.";Database=".$db_name;   
+
+        $db_user = getenv('DB_USERNAME');         
+        $db_password = getenv('DB_PASSWORD');   
+
+        $db = new PDO($db_url, $db_user, $db_password); 
+	} catch (PDOException $e) {
+		echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+		exit;
+	}
+
+	$dao = new SqlsrvDao($db);
+	
+	$lastDate = getenv('Last_Date');
+	$ch = getenv('CHANNEL');
+
+	$ch_title = getenv('CH_TITLE');
+	$init_dt = '';
+	
+	$s_time=$s_time[$ch];	
+	
+	if (!$lastDate) {
+		$init_dt='2018-05-01 00:00:00';
+	}else
+	{
+		$init_dt=$lastDate.' 00:00:00';
+	}
+
+	$nextDate =  Carbon::createFromFormat('Y-m-d H:i:s', $init_dt)->addDay();
+	$toDay= Carbon::now()->toDateString();
+
+	while ($nextDate<$toDay) {		
+		$filename = getenv('LOG_PATH').'XBlog['.$ch.'--主机]'.$nextDate->format('Ymd').'.txt';
+		//返回插播的素材，播出时间，素材长度列表
+		$clipFiles = LogFile::getInstance()->toArray($filename);
+
+		//从数据库中获取合同的信息	
+		$contracts=collect($dao->getADInfoByClipFiles($clipFiles));
+
+		$filename=getenv('OUTPUT_PATH').$nextDate->format('Ymd').$ch.'.txt';
+		$str='';
+		
+		$contracts->each(function ($contract) use (&$str, $nextDate, $ch_title, $s_time) {
+			//计算广告长度
+			$len = intval($contract['len']);
+			$ilen = (int)floor($len/25);
+
+			$h=(int)floor($ilen/3600);
+			$m=(int)floor(($ilen%3600)/60);
+			$s=$ilen-$h*3600-$m*60;		
+			$f=(int)($len%25);
+
+			$slen=str_pad($h,2,'0',STR_PAD_LEFT).str_pad($m,2,'0',STR_PAD_LEFT).str_pad($s,2,'0',STR_PAD_LEFT).str_pad($f,2,'0',STR_PAD_LEFT);
+			
+			//根据播出时间，计算得出时间段
+			$temp=substr($contract['b_time'],0,6);
+			$s='';			
+			foreach ($s_time as $key=>$val) {
+				if (($temp>=$val[0]) && ($temp>=$val[1])) {	
+					$s=$key;
+					break;
+				}
+			}
+
+			$line=$nextDate->format('Ymd').substr($contract['b_time'],0,8).$slen.str_pad($ch_title, 16, " ").str_pad($s, 16, " ").$contract['strdescription']."\r\n";
+			$str=$str.$line;
+		});
+		//dd($contracts);
+		if ($str) {
+			file_put_contents($filename, $str, LOCK_EX);	
+			$env->set([
+				'Last_Date'=>$nextDate->format('Y-m-d')
+			]);		
+		}
+		
+		//fclose($file);
+		$nextDate = $nextDate->addDay();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>首页 - 汕头市广播电视台</title>
+
+    <!-- Styles -->
+    <link href="./public/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app" class="root-page">
+
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="#">
+                        XBLog
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-user"></i> <?php echo $_SESSION['userinfo']['name'] ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="button" href="logout.php" onclick=" return confirm('你确定要退出吗?')">
+                                <i class="fa fa-sign-out"></i> 注销
+                            </a>
+                        </li>
+
+                     </ul>
+                </div>
+            </div>
+        </nav>
+        
+
+        <div class="container">
+
+        	<div class="jumbotron">
+			    <h3>XBlog</h3>
+			    <p class="lead">
+			      播后数据执行成功。
+			    </p>			   
+			    <p>
+			      <a class="btn btn-lg btn-success" href="./main.php" role="button">返回</a>
+			    </p>
+			 </div>
+            
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">
+                    由 XMAN 设计和编码
+                </p>
+
+                <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+            </div>
+        </footer> 
+    </div>
+
+
+
